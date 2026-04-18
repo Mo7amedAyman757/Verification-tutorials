0c309b6ed73cb98a35060b78a378473f891002c1
Mo7amedAyman757 <ma5550236@gmail.com>
2026-04-18 22:28:25 +0200

    Adding Vending Machine test using Interface

--- Day4/Vending_machine/vending_machine_sva.sv
+++ Day4/Vending_machine/vending_machine_sva.sv
@@ -6,17 +6,18 @@
 // 
 ////////////////////////////////////////////////////////////////////////////////
 module vending_machine_sva(vending_machine_if.DUT v_if);
-// 1. Add the modport above
-// 2. Add the following 3 properties, then use assert property and cover property on each property
-//// First Assertion: At each positive edge of the clock, if the D_in is high then at the same clock cycle, the dispense and the change outputs are high
+
+    // 1. Add the modport above
+    // 2. Add the following 3 properties, then use assert property and cover property on each property
+    //// First Assertion: At each positive edge of the clock, if the D_in is high then at the same clock cycle, the dispense and the change outputs are high
     property p_dollar;
         @(posedge v_if.clk) v_if.D_in |-> (v_if.dispense && v_if.change);
     endproperty
-//// Second Assertion: At each positive edge of the clock, If there is a rising edge for the input Q_in then after 2 clock cycles the dispense output is high
+    // Second Assertion: At each positive edge of the clock, If there is a rising edge for the input Q_in then after 2 clock cycles the dispense output is high
     property p__quarter_dispense;
         @(posedge v_if.clk) $rose(v_if.Q_in) |-> ##2 v_if.dispense;
     endproperty
-//// Third Assertion: At each positive edge of the clock, if the Q_in is high then at the same clock cycle, the change must be low
+    // Third Assertion: At each positive edge of the clock, if the Q_in is high then at the same clock cycle, the change must be low
     property p_quarter_change;
         @(posedge v_if.clk) ($rose(v_if.Q_in) |-> !(v_if.change));    
     endproperty
